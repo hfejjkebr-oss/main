e7c9bee8f9231f236bfc27d113826077814205c9
hfejjkebr-oss <hfejjkebr@gmail.com>
2026-08-29 19:28:49 +0700

    Add Seihub.lua with script loading functionality

---
+++ Seihub.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/92fe0df52bc373a3cefb625ca214cd7e93ba3a895a67e6f0901c66f5b7d94a96/download"))()
