d82fbee6dc38852831574ed0fc79e76aae724a19
hfejjkebr-oss <hfejjkebr@gmail.com>
2026-09-20 12:14:22 +0700

    Create Stal_An_Egg.lua

---
+++ Stal_An_Egg.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bbb6d929fd2aa4dc901c81ce53ef1a4360b1ed0e4bb6cbe386ccb9eb4393c03c/download"))()
